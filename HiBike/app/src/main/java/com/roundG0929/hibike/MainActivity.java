f7731eaacdc26fa7f8b6f134a545e281307e2b74
eui9179 <outdark97@gmail.com>
2022-02-15 17:08:49 +0900

    put-login-button-in-navigation

--- HiBike/app/src/main/java/com/roundG0929/hibike/MainActivity.java
+++ HiBike/app/src/main/java/com/roundG0929/hibike/MainActivity.java
@@ -2,28 +2,55 @@ package com.roundG0929.hibike;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.roundG0929.hibike.activities.auth.SigninActivity;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
-    Button mainSigninBtn;
+
+    TextView btn_signin;
+    ImageButton btn_open;
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mainSigninBtn = (Button) findViewById(R.id.mainSigninBtn);
-        mainSigninBtn.setOnClickListener(new View.OnClickListener() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerView = (View) findViewById(R.id.drawer);
+
+        btn_open = (ImageButton) findViewById(R.id.btn_open);
+        btn_open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
+
+        drawerLayout.setDrawerListener(listener);
+        drawerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return true;
+            }
+        });
+
+        btn_signin = (TextView) findViewById(R.id.btn_signin);
+        btn_signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), SigninActivity.class);
@@ -32,4 +59,26 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+
+        }
+    };
 }
--- HiBike/app/src/main/java/com/roundG0929/hibike/activities/auth/SigninActivity.java
+++ HiBike/app/src/main/java/com/roundG0929/hibike/activities/auth/SigninActivity.java
@@ -2,12 +2,16 @@ package com.roundG0929.hibike.activities.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.roundG0929.hibike.MainActivity;
 import com.roundG0929.hibike.R;
 import com.roundG0929.hibike.api.server.RetrofitClient;
 import com.roundG0929.hibike.api.server.dto.SigninDto;
@@ -20,23 +24,23 @@ import retrofit2.Response;
 public class SigninActivity extends AppCompatActivity {
 
     ApiInterface api;
-    Button signinBtn;
+    Button btn_signin;
     EditText editId;
     EditText editPwd;
-    TextView signupText;
+    TextView textSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
         api = RetrofitClient.getRetrofit().create(ApiInterface.class);
-        editId = (EditText) findViewById(R.id.editId);
-        editPwd = (EditText) findViewById(R.id.editPassword);
-        signupText = (TextView) findViewById(R.id.signup);
+        editId = (EditText) findViewById(R.id.edit_id);
+        editPwd = (EditText) findViewById(R.id.edit_password);
+        textSignup = (TextView) findViewById(R.id.text_signup);
 
-        signinBtn = (Button) findViewById(R.id.signinBtn);
+        btn_signin = (Button) findViewById(R.id.btn_signin);
 
-        signinBtn.setOnClickListener(new View.OnClickListener() {
+        btn_signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SigninDto data = new SigninDto();
@@ -46,17 +50,20 @@ public class SigninActivity extends AppCompatActivity {
                 api.signin(data).enqueue(new Callback<SigninDto>() {
                     @Override
                     public void onResponse(Call<SigninDto> call, Response<SigninDto> response) {
-                        if(response.isSuccessful()){
-                            signupText.setText("success");
-                        }else{
-                            signupText.setText(response.message());
+                        if (response.isSuccessful()) {
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast toast = Toast.makeText(getApplicationContext(), "로그인 정보가 일치하지 않습니다. (" + response.message() + ")", Toast.LENGTH_LONG);
+//                            toast.setGravity(Gravity.TOP|Gravity.LEFT, 200, 200);
+                            toast.show();
                         }
                     }
 
                     @Override
                     public void onFailure(Call<SigninDto> call, Throwable t) {
                         //통신 실패
-                        signupText.setText(t.toString());
+                        Toast.makeText(getApplicationContext(), t.toString(), Toast.LENGTH_LONG).show();
                     }
                 });
             }
