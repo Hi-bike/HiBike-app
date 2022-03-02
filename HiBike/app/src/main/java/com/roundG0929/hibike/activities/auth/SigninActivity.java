904caa3105021e84cd5c53ba661c5324f42f0e9d
limsu123 <77198387+limsu123@users.noreply.github.com>
2022-03-03 05:56:48 +0900

    로그인 회원가입

--- HiBike/app/src/main/java/com/roundG0929/hibike/activities/auth/SigninActivity.java
+++ HiBike/app/src/main/java/com/roundG0929/hibike/activities/auth/SigninActivity.java
@@ -1,15 +1,24 @@
 package com.roundG0929.hibike.activities.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.Html;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,7 +37,6 @@ public class SigninActivity extends AppCompatActivity {
     Button btnSignin;
     EditText editId, editPwd;
     TextView textSignup;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -87,5 +95,29 @@ public class SigninActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //내용이 있을 때만 버튼 색 활성화
+        btnSignin.setClickable(false);
+        btnSignin.setBackgroundColor(Color.parseColor("#DDDDDD"));
+
+        editPwd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                btnSignin.setClickable(true);
+                btnSignin.setBackgroundColor(Color.parseColor("#6CD1FF"));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+        //회원가입 밑줄
+        textSignup.setPaintFlags(textSignup.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        ImageView imageView;
     }
+
 }
