edb6ec5f183631bbc20937e82c10e7715ddc34fb
SteelZoo <cjfwn96@gmail.com>
2022-03-07 15:11:22 +0900

    save before pull main

---
+++ HiBike/app/src/main/java/com/roundG0929/hibike/activities/map_route/SearchPlaceActivity.java
@@ -0,0 +1,18 @@
+package com.roundG0929.hibike.activities.map_route;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.roundG0929.hibike.R;
+
+public class SearchPlaceActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchplace);
+
+
+    }
+}
