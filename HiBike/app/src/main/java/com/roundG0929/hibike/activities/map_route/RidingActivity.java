4e5f90d8f9960dcdeb81bf9196369fa5b25b2732
SteelZoo <cjfwn96@gmail.com>
2022-04-05 16:03:05 +0900

    make riding activity, layout

--- HiBike/app/src/main/java/com/roundG0929/hibike/activities/map_route/RidingActivity.java
+++ HiBike/app/src/main/java/com/roundG0929/hibike/activities/map_route/RidingActivity.java
@@ -1,13 +1,91 @@
 package com.roundG0929.hibike.activities.map_route;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentManager;
 
-public class RidingActivity extends AppCompatActivity {
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.util.FusedLocationSource;
+import com.roundG0929.hibike.R;
+import com.roundG0929.hibike.api.map_route.navermap.MapSetting;
+
+public class RidingActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    NaverMap naverMapObj;
+    MapFragment mapFragment;
+    private FusedLocationSource fusedLocationSource;
+    private static final int NAVER_LOCATION_PERMISSION_CODE = 1000;
+    MapSetting mapSetting = new MapSetting();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_riding);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        }
+
+
+        //초기맵설정
+        //맵 뷰 객체 할당
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            fragmentManager.beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+        //위치소스
+        fusedLocationSource = new FusedLocationSource(this, NAVER_LOCATION_PERMISSION_CODE);
+        //맵객체 설정
+        mapFragment.getMapAsync(this::onMapReady);
+    }
+
+
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        naverMapObj = naverMap;
+
+        mapSetting.routeActivityMapSet(naverMapObj,getApplicationContext(),fusedLocationSource);
+    }
+
+
+    //위경도 사용 거리 구하는 메소드
+    private static double distance(double lat1, double lon1, double lat2, double lon2, String unit) {
+
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+
+        if (unit == "kilometer") {
+            dist = dist * 1.609344;
+        } else if(unit == "meter"){
+            dist = dist * 1609.344;
+        }
+
+        return (dist);
+    }
+
+
+    // This function converts decimal degrees to radians
+    private static double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    // This function converts radians to decimal degrees
+    private static double rad2deg(double rad) {
+        return (rad * 180 / Math.PI);
     }
 }
